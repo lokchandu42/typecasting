ab5ae9493ef69d6f4665601622cb548cf985f556
lokchandu42 <98245175+lokchandu42@users.noreply.github.com>
2022-01-27 15:31:51 +0530

    Add files via upload

---
+++ typecasting/src/com/typecasting/Implisittypecasting.java
@@ -0,0 +1,18 @@
+package com.typecasting;
+
+public class Implisittypecasting {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 7;
+		long y = x;
+		float z = y;
+		System.out.println("before conversion, int value" +x);
+		System.out.println("after conversion, long value" +y);
+		System.out.println("before conversion, float value" +z);
+		
+
+	}
+
+}
---
+++ typecasting/src/com/typecasting/explisittypecasting.java
@@ -0,0 +1,18 @@
+package com.typecasting;
+
+public class explisittypecasting {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double d = 166.22;
+		long l = (long)d;
+		int i = (int)l;
+		System.out.println("before conversion: "+d);
+		System.out.println("after conversion into long type: "+l);
+		System.out.println("after conversion into int type: "+i);
+		
+
+	}
+
+}
